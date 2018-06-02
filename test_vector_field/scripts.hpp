31f0749e1f491e389230146998138524479f243c
Léni Le Goff <lenilegoff@gmail.com>
2018-06-02 15:37:43 +0200

    simplify test on vector field

--- test_vector_field/scripts.hpp
+++ test_vector_field/scripts.hpp
@@ -48,8 +48,8 @@ static std::function<void(age::Entity*)> attractor = [](age::Entity* self){
 
                 vel = TO_DOUBLE(self->access_property()["velocity"])->value*cos(PI/4.);
 
-                p.emplace("x",DOUBLE_PTR(vel/**cos(calc_angle(x,y,xself,yself))*/));
-                p.emplace("y",DOUBLE_PTR(vel/**sin(calc_angle(x,y,xself,yself))*/));
+                p.emplace("x",DOUBLE_PTR(vel*cos(calc_angle(x,y,xself,yself))));
+                p.emplace("y",DOUBLE_PTR(vel*sin(calc_angle(x,y,xself,yself))));
             }
             else {
                 p.emplace("x",DOUBLE_PTR(0));
@@ -59,7 +59,7 @@ static std::function<void(age::Entity*)> attractor = [](age::Entity* self){
             vf[i][j] = p;
         }
     }
-//    emit self->send_vector_field(TO_STRING(self->access_property()["name"])->value,vf);
+    emit self->send_vector_field(TO_STRING(self->access_property()["name"])->value,vf);
 
 };
 
@@ -79,12 +79,12 @@ static std::function<void(const age::_property_t&, age::_property_t&)> adjust_an
 
 //GLOBAL SCRIPTS
 inline void create_blurg(const std::string& name, age::Engine::Ptr engine){
-    age::Entity::Ptr new_ent(new age::Entity("blurg",10,10,9));
+    age::Entity::Ptr new_ent(new age::Entity("blurg",10,10,3));
     new_ent->add_property("state",STRING_PTR("red"));
     new_ent->add_property("life",DOUBLE_PTR(50));
     new_ent->add_property("sprite",STRING_PTR("sprite_1"));
-    new_ent->set_property("x",DOUBLE_PTR(rand()%700 + 50));
-    new_ent->set_property("y",DOUBLE_PTR(rand()%500 + 50));
+    new_ent->set_property("x",DOUBLE_PTR(300));
+    new_ent->set_property("y",DOUBLE_PTR(300));
     boost::random::uniform_int_distribution<> dist(-PI,PI);
     new_ent->set_property("angle",DOUBLE_PTR(0));
     new_ent->set_collider(new age::CircleCollider(10));
@@ -94,15 +94,15 @@ inline void create_blurg(const std::string& name, age::Engine::Ptr engine){
 }
 
 inline void create_attractor(const std::string& name, age::Engine::Ptr engine){
-    age::Entity::Ptr new_ent(new age::Entity(name,10,10,10));
+    age::Entity::Ptr new_ent(new age::Entity(name,10,10,3));
     new_ent->add_property("state",STRING_PTR("blue"));
     new_ent->add_property("life",DOUBLE_PTR(50));
     new_ent->add_property("sprite",STRING_PTR("sprite_1"));
-    new_ent->set_property("x",DOUBLE_PTR(rand()%400 + 200));
-    new_ent->set_property("y",DOUBLE_PTR(rand()%300+150));
+    new_ent->set_property("x",DOUBLE_PTR(400));
+    new_ent->set_property("y",DOUBLE_PTR(300));
     boost::random::uniform_int_distribution<> dist(-PI,PI);
     new_ent->set_property("angle",DOUBLE_PTR(0));
-    new_ent->set_collider(new age::CircleCollider(100));
+    new_ent->set_collider(new age::CircleCollider(1000));
     new_ent->set_custom_run(attractor);
 //    new_ent->set_behavior(go_forward);
 //    new_ent->set_in_collision(adjust_angle);
@@ -114,24 +114,17 @@ inline void create_attractor(const std::string& name, age::Engine::Ptr engine){
 
 inline void init_test(age::Engine::Ptr engine,age::AnimatedManager am){
 
-    for(int i = 1; i < 10; i++){
-        std::stringstream key_name;
-        key_name << "attractor_" << i;
-        create_attractor(key_name.str(),engine);
+    create_attractor("attractor",engine);
 
-        engine->get_entities()[key_name.str()]->link_to_sprite(am.get("sprite_1"));
-//        engine->get_entities()[key_name.str()]->set_behavior(go_forward);
+    engine->get_entities()["attractor"]->link_to_sprite(am.get("sprite_1"));
 
-    }
-    for(int i = 1; i < 2; i++){
-        std::stringstream key_name;
-        key_name << "blurg_" << i;
-        create_blurg(key_name.str(),engine);
 
-        engine->get_entities()[key_name.str()]->link_to_sprite(am.get("sprite_1"));
-//        engine->get_entities()[key_name.str()]->set_behavior(go_forward);
 
-    }
+    create_blurg("blurg",engine);
+
+    engine->get_entities()["blurg"]->link_to_sprite(am.get("sprite_1"));
+
+
     engine->_run();
 }
 
